82a3df68d1e28150bdea1b9a15c8269a985876d2
Ryan <ryan@thecodestress.com>
2017-01-16 16:06:08 -0500

    Continued Binary tree practice: Added function to test if tree is balanced.

--- Java/BinaryTreePractice/src/com/thecodestress/BinaryTree.java
+++ Java/BinaryTreePractice/src/com/thecodestress/BinaryTree.java
@@ -1,51 +1,72 @@
 package com.thecodestress;
 
 /**
+ * Simple binary tree
+ *
  * Created by RedDevil on 1/12/2017.
  */
-
 public class BinaryTree {
 
     Node root;
 
     /**
-     * Function to determine the height of binary tree
-     *
+     * Constructor sets the root node to null
      */
-    public int height(){
-        return heightUtil(root);
+
+    public BinaryTree() {
+        root = null;
     }
 
-    private int heightUtil(Node node){
+    /**
+     * Functions to determine the height of binary tree
+     */
+    public int height() {
+        return height(root);
+    }
 
-        if (node == null){
+    public int height(Node node) {
+        if (node == null) {
             return 0;
         }
-
-        return 1 + Math.max(heightUtil(node.left), heightUtil(node.right));
+        return 1 + Math.max(height(node.left), height(node.right));
     }
 
     /**
      * Function to count the number nodes in binary tree
-     *
      */
-    public int numberOfNodes(){
-        return numberOfNodesUtil(root);
+    public int numberOfNodes() {
+        return numberOfNodes(root);
     }
 
-    private int numberOfNodesUtil(Node node){
-
-        if (node == null){
+    public int numberOfNodes(Node node) {
+        if (node == null) {
             return 0;
         }
-
-        return 1 + numberOfNodesUtil(node.left) + numberOfNodesUtil(node.right);
+        return 1 + numberOfNodes(node.left) + numberOfNodes(node.right);
     }
 
     /**
      * Function to determine if a binary tree is balanced
-     *
      */
-    //public boolean isBalanced(){}
+    public boolean isBalanced() {
+        return isBalanced(root);
+    }
+
+    public boolean isBalanced(Node node) {
+
+        if (node == null) {
+            return true;
+        }
+
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+        int difference = Math.abs(leftHeight - rightHeight);
+
+        if ((difference <= 1) && isBalanced(node.left) && isBalanced(node.right)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
--- Java/BinaryTreePractice/src/com/thecodestress/Main.java
+++ Java/BinaryTreePractice/src/com/thecodestress/Main.java
@@ -15,13 +15,23 @@ package com.thecodestress;
 public class Main {
 
     public static void main(String[] args) {
+
         BinaryTree tree = new BinaryTree();
+        System.out.println("Is tree balanced: " + tree.isBalanced());
+
         tree.root = new Node(0);
         tree.root.left = new Node(1);
         tree.root.right = new Node(2);
         tree.root.left.left = new Node(3);
         tree.root.left.right = new Node(4);
+        tree.root.right.left = new Node(5);
+        tree.root.left.right.left = new Node(6);
+        tree.root.left.right.left.right = new Node(7);
 
-        System.out.println(tree.numberOfNodes());
+        System.out.println("Total number of nodes: " + tree.numberOfNodes());
+        System.out.println("Height of root: " + tree.height());
+        System.out.println("Height of left subtree: " + tree.height(tree.root.left));
+        System.out.println("Height of right subtree: " + tree.height(tree.root.right));
+        System.out.println("Is tree balanced: " + tree.isBalanced());
     }
 }
